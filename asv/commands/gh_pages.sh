cbbef36ca3fc876e2ec91a3e161b3219e1bedae5
Michael Droettboom <mdboom@gmail.com>
2013-12-11 12:39:30 -0500

    Fix gh-pages command

--- asv/commands/gh_pages.sh
+++ asv/commands/gh_pages.sh
@@ -6,19 +6,12 @@ set -e
 # the built products
 git checkout --orphan gh-pages
 
-# This will delete all of the git-managed files here, but not
-# the results of the build
-git rm -rf .
-# Copy the built files to the root
-cp -r ${HTML_DIR}/* .
-
-# Delete the original location of the built files
-rm -rf ${HTML_DIR}
-
 # We need to tell github this is not a Jekyll document
 touch .nojekyll
 git add .nojekyll
-git add *
+git add -f html
+git mv html/* .
 git commit -m "Generated from sources"
 
 git push -f origin gh-pages
+git checkout master
