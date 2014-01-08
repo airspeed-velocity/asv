30f78cc96f56445e435c3af3d77b48dac8fce4c5
Michael Droettboom <mdboom@gmail.com>
2014-01-08 14:11:23 -0500

    Try to get more info out of Travis

--- asv/util.py
+++ asv/util.py
@@ -228,7 +228,7 @@ def check_output(args, error=True, timeout=120, dots=True, display_error=True,
     retcode = proc.wait()
     if retcode:
         if error:
-            if display_error:
+            if display_error or True:
                 console.error("Running {0}".format(" ".join(args)))
                 console.add("STDOUT " + ("-" * 60) + '\n', 'red')
                 console.add(stdout)
