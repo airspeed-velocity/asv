933a082a76c6c9b72aaf275f45f0d155f66eeacf
Michael Droettboom <mdboom@gmail.com>
2014-01-10 10:21:22 -0500

    Fix Python 3.3 calling another virtualenv as a subprocess.

--- asv/__init__.py
+++ asv/__init__.py
@@ -3,3 +3,12 @@
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+
+import sys
+
+if sys.version_info >= (3, 3):
+    # OS X framework builds of Python 3.3 can not call other 3.3
+    # virtualenvs as a subprocess because `__PYENV_LAUNCHER__` is
+    # inherited.
+    if os.environ.get('__PYVENV_LAUNCHER__'):
+        os.unsetenv('__PYVENV_LAUNCHER__')
