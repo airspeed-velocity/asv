b03a08fcff6407aec63aa327685750ee28072ebd
Michael Droettboom <mdboom@gmail.com>
2014-08-08 15:03:57 -0400

    Add a test for custom plugin loading.

--- test/test_conf.py
+++ test/test_conf.py
@@ -4,9 +4,14 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import os
 from os.path import dirname, join
 
 from asv import config
+from asv import commands
+from asv.commands import Command
+from asv import environment
+from asv import util
 
 
 def test_config():
@@ -17,3 +22,37 @@ def test_config():
         "numpy": ["1.8"],
         "Cython": []
     }
+
+
+class CustomCommand(Command):
+    @classmethod
+    def setup_arguments(cls, subparsers):
+        parser = subparsers.add_parser(
+            "custom", help="Custom command",
+            description="Juts a test.")
+
+        parser.set_defaults(func=cls.run_from_args)
+
+        return parser
+
+    @classmethod
+    def run_from_conf_args(cls, conf, args):
+        pass
+
+
+def test_load_plugin():
+    os.chdir(os.path.dirname(__file__))
+
+    parser, subparsers = commands.make_argparser()
+    args = parser.parse_args(['custom'])
+
+    assert hasattr(args, 'func')
+
+    args.func(args)
+
+    for env in util.iter_subclasses(environment.Environment):
+        print(env.__name__)
+        if env.__name__ == 'MyEnvironment':
+            break
+    else:
+        assert False, "Custom plugin not loaded"
