bf8177cc92f0956e75c5edb86893f2c0102d825f
Michael Droettboom <mdboom@gmail.com>
2014-12-09 13:15:56 -0500

    Fix UnboundLocalError

--- asv/benchmarks.py
+++ asv/benchmarks.py
@@ -106,9 +106,9 @@ def run_benchmark(benchmark, root, env, show_stderr=False, quick=False,
                         with io.open(profile_path, 'rb') as profile_fd:
                             result['profile'] = profile_fd.read()
 
-            if show_stderr and err.strip():
-                with log.indent():
-                    log.error(err.strip())
+                if show_stderr and err.strip():
+                    with log.indent():
+                        log.error(err.strip())
 
             return result
         finally:
