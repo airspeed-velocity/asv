c966287f33ef3e0e1c61189c1a239d952c3189fe
Michael Droettboom <mdboom@gmail.com>
2014-12-16 15:25:08 -0500

    Don't have Conda do any network fetching

--- asv/plugins/conda.py
+++ asv/plugins/conda.py
@@ -98,7 +98,9 @@ class Conda(environment.Environment):
                 '--yes',
                 '-p',
                 self._path,
-                'python={0}'.format(self._python), 'pip'])
+                '--use-index-cache',
+                'python={0}'.format(self._python),
+                'pip'])
         except util.ProcessError:
             log.error("Failure creating conda environment for {0}".format(
                 self.name))
