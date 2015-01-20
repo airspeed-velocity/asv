1bc61edde0e41ec3f2fe66758654b55ed51ec36a
Kacper Kowalik <xarthisius.kk@gmail.com>
2015-01-20 15:18:47 -0600

    Add test for mercurial repo

--- test/test_repo.py
+++ test/test_repo.py
@@ -10,18 +10,17 @@ from asv import config
 from asv import repo
 
 
-def test_repo(tmpdir):
-    conf = config.Config()
-
-    conf.project = six.text_type(tmpdir.join("repo"))
-    conf.repo = "https://github.com/spacetelescope/asv.git"
+def _test_generic_repo(conf,
+                       hash_range="ae0c27b65741..e6f382a704f7",
+                       master="master",
+                       branch="gh-pages"):
 
     r = repo.get_repo(conf)
-    r.checkout("master")
-    r.checkout("gh-pages")
-    r.checkout("master")
+    r.checkout(master)
+    r.checkout(branch)
+    r.checkout(master)
 
-    hashes = r.get_hashes_from_range("ae0c27b65741..e6f382a704f7")
+    hashes = r.get_hashes_from_range(hash_range)
     assert len(hashes) == 4
 
     dates = [r.get_date(hash) for hash in hashes]
@@ -30,3 +29,20 @@ def test_repo(tmpdir):
     tags = r.get_tags()
     for tag in tags:
         r.get_date_from_tag(tag)
+
+
+def test_repo_git(tmpdir):
+    conf = config.Config()
+
+    conf.project = six.text_type(tmpdir.join("repo"))
+    conf.repo = "https://github.com/spacetelescope/asv.git"
+    _test_generic_repo(conf)
+
+
+def test_repo_hg(tmpdir):
+    conf = config.Config()
+
+    conf.project = six.text_type(tmpdir.join("repo"))
+    conf.repo = "hg+https://bitbucket.org/nds-org/nds-labs"
+    _test_generic_repo(conf, hash_range="a8ca24ac6b77:9dc758deba8",
+                       master="tip", branch="dev")
