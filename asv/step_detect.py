ee2c229b1c2a712232eca4242252b0201187700f
Pauli Virtanen <pav@iki.fi>
2015-03-03 22:28:03 +0200

    Add routines for fitting piecewise continuous functions to data with jump penalty

---
+++ asv/step_detect.py
@@ -0,0 +1,510 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, unicode_literals, print_function
+
+import math
+import heapq
+import six
+
+
+#
+# Detecting regressions
+#
+
+def detect_regressions(y, size_limit=20000):
+    """
+    Detect newest upward steps in a (noisy) signal.
+
+    Parameters
+    ----------
+    y : list of float, none or nan
+        Single benchmark result series, with possible missing data
+    size_limit : int, optional
+        Consider only this many recent commits (default: 20000)
+
+    Returns
+    -------
+    regressions : list of 3-tuples (pos, old_value, new_value)
+        List of positions of upward steps in the data since the
+        global minimum. For each step, old and new values are
+        included. This is an empty list, if the most recent
+        values are also the smallest ones.
+
+    """
+
+    index_map = {}
+    y_filtered = []
+    for j, x in enumerate(y):
+        if x is None or x != x:
+            # None or NaN: missing data
+            continue
+        index_map[len(y_filtered)] = j
+        y_filtered.append(x)
+
+    right, values, dists, gamma = solve_potts_autogamma(y_filtered)
+
+    prev_r = None
+    prev_v = None
+    prev_min = None
+
+    regressions = []
+    for r, v in zip(right, values):
+        if prev_min is None:
+            prev_min = v
+
+        if prev_v is not None:
+            if v > prev_v:
+                regressions.append((index_map[prev_r], prev_v, v))
+            elif v < prev_min:
+                del regressions[:]
+        prev_r, prev_v = r, v
+
+    return regressions
+
+
+#
+# Fitting piecewise constant functions to noisy data
+#
+
+def solve_potts(y, gamma, p=2, min_size=2, max_size=1e99,
+                min_pos=None, max_pos=None, mu_dist=None):
+    """Fit penalized stepwise constant function (Potts model) to data.
+
+    Given a time series y = {y_1, ..., y_n}, fit series x = {x_1, ..., x_n}
+    by minimizing the cost functional::
+
+        F[x] = gamma * J(x) + sum(|y - x|**p)
+
+    where J(x) is the number of jumps (x_{j+1} != x_j) in x.
+
+    The algorithm used is described in Ref. [1]_, it uses dynamic
+    programming to find an exact solution to the problem (within the
+    constraints specified).
+
+    For p=2 norm, the performance is O(n**2), or
+    O((max_pos-min_pos)*(min_size-max_size+1)) if constrained.
+
+    For p=1 norm, it is O(n**2 log n) and the constants in front are
+    bigger, due to the somewhat sub-optimal (= in Python) way median
+    and absolute sum computations are done.
+
+    Parameters
+    ----------
+    y : list of floats
+        Input data series
+    gamma : float
+        Penalty parameter.
+    min_size : int, optional
+        Minimum interval size to consider
+    max_size : int, optional
+        Maximum interval size to consider
+    mu_dist : MuDist, optional
+        Precomputed interval means/medians and cost function values
+    min_pos : int, optional
+        Start point (inclusive) for the interval grid
+    max_pos : int, optional
+        End point (exclusive) for the interval grid
+
+    Returns
+    -------
+    right : list
+        List of (exclusive) right bounds of the intervals
+    values : list
+        List of values of the intervals
+    dist : list
+        List of ``sum(|y - x|**p)`` for each interval.
+    mu_dist : MuDist
+        Precomputed interval means/medians and cost function values
+
+    References
+    ----------
+    [1] F. Friedrich et al., "Complexity Penalized M-Estimation: Fast Computation",
+        Journal of Computational and Graphical Statistics 17.1, 201-224 (2008).
+
+    """
+
+    inf = float('inf')
+
+    if len(y) == 0:
+        return [], [], []
+
+    if min_pos is None:
+        min_pos = 0
+
+    if max_pos is None:
+        max_pos = len(y)
+
+    if mu_dist is None:
+        mu_dist = MuDist(y, p)
+
+    mu_dist.precompute(max_size, min_pos, max_pos)
+    mu, dist = mu_dist.get_funcs()
+
+    if min_size >= max_pos - min_pos:
+        return [len(y)], [mu(0,len(y)-1)], [dist(0,len(y)-1)]
+
+    def find_best_partition(n, gamma, dist):
+        """
+        Perform the Bellman recursion for the optimal partition.
+
+        Returns
+        -------
+        p : list, length n
+            Set of intervals, represented as follows:
+            For interval (inclusive) right edge r in {0, ..., n-1},
+            the best (exclusive) left edge is at l=p[r].
+            Where intervals overlap, the rightmost one has priority.
+
+        """
+        i0 = min_pos
+        i1 = max_pos
+
+        B = [-gamma]
+        p = [None]*i1
+        for r in range(i0, i1):
+            B.append(inf)
+            a = max(r + 1 - max_size, i0)
+            b = max(r + 1 - min_size + 1, i0)
+            for l in range(a, b):
+                b = B[l-i0] + gamma + dist(l, r)
+                if b <= B[r+1-i0]:
+                    B[r+1-i0] = b
+                    p[r] = l - 1
+        return p
+
+
+    def segmentation_from_partition(n, p, mu, dist):
+        """
+        Convert interval representation computed by find_best_partition
+        to a list of intervals and values.
+        """
+        i0 = min_pos
+        r = len(p) - 1
+        l = p[r]
+        right = []
+        values = []
+        dists = []
+        while r >= i0:
+            right.append(r + 1)
+            values.append(mu((l + 1), r))
+            dists.append(dist((l + 1), r))
+            r = l
+            l = p[r]
+        right.reverse()
+        values.reverse()
+        dists.reverse()
+        return right, values, dists
+
+
+    n = len(y)
+    p = find_best_partition(n, gamma, dist)
+    return segmentation_from_partition(n, p, mu, dist)
+
+
+def solve_potts_autogamma(y, beta=0.025, **kw):
+    """Solve Potts problem with automatically determined gamma.
+
+    The optimal value is determined by minimizing the information measure::
+
+        f(gamma) = beta J(x(gamma)) + log sum(abs(x(gamma) - y)**p)
+
+    where x(gamma) is the solution to the Potts problem for a fixed
+    gamma. The minimization is only performed rather roughly.
+
+    """
+    n = len(y)
+
+    if n == 0:
+        return [], [], [], None
+
+    mu_dist = MuDist(y, kw.get('p', 2))
+    mu, dist = mu_dist.get_funcs()
+
+    gamma_0 = dist(0, n-1)
+
+    best_r = [None]
+    best_v = [None]
+    best_d = [None]
+    best_obj = [float('inf')]
+    best_gamma = [None]
+
+    def f(x):
+        gamma = gamma_0 * math.exp(x)
+        r, v, d = solve_potts_approx(y, gamma=gamma, mu_dist=mu_dist, **kw)
+        obj = beta*len(r) + math.log(1e-300 + sum(d))
+        if obj < best_obj[0]:
+            best_r[0] = r
+            best_v[0] = v
+            best_d[0] = d
+            best_gamma[0] = gamma
+            best_obj[0] = obj
+        return obj
+
+    # Try to find best gamma (golden section search on log-scale); we
+    # don't need an accurate value for it however
+    a = math.log(0.1/n)
+    b = 0
+    golden_search(f, a, b, xatol=abs(a)*0.1, ftol=0.01)
+    return best_r[0], best_v[0], best_d[0], best_gamma[0]
+
+
+def solve_potts_approx(y, gamma=None, p=2, **kw):
+    """
+    Fit penalized stepwise constant function (Potts model) to data
+    approximatively, in linear time.
+
+    Do this by running the exact solver using a small maximum interval
+    size, and then combining consecutive intervals together if it
+    decreases the cost function.
+    """
+    n = len(y)
+
+    if n == 0:
+        return [], [], []
+
+    mu_dist = kw.get('mu_dist')
+    if mu_dist is None:
+        mu_dist = MuDist(y, p=p)
+        kw['mu_dist'] = mu_dist
+
+    if gamma is None:
+        mu, dist = mu_dist.get_funcs()
+        gamma = 3 * dist(0,n-1) * math.log(n) / n
+
+    right, values, dists = solve_potts(y, gamma, p=p, max_size=20, **kw)
+    return merge_pieces(gamma, right, values, dists, mu_dist)
+
+
+def merge_pieces(gamma, right, values, dists, mu_dist):
+    """
+    Combine consecutive intervals in Potts model solution, if doing
+    that reduces the cost function.
+    """
+    mu, dist = mu_dist.get_funcs()
+
+    right = list(right)
+    values = list(values)
+    dists = list(dists)
+
+    while True:
+        min_change = 0
+        min_change_j = len(right)
+
+        l = 0
+        for j in range(1, len(right)):
+            if min_change_j < j - 2:
+                break
+
+            # Check whether merging consecutive intervals results to
+            # decrease in the cost function
+            change = dist(l, right[j]-1) - (dist(l, right[j-1]-1) + dist(right[j-1], right[j]-1) + gamma)
+            if change <= min_change:
+                min_change = change
+                min_change_j = j-1
+            l = right[j-1]
+
+        if min_change_j < len(right):
+            del right[min_change_j]
+        else:
+            break
+
+    l = 0
+    for j in range(len(right)):
+        dists[j] = dist(l, right[j]-1)
+        values[j] = mu(l, right[j]-1)
+        l = right[j]
+
+    return right, values, dists
+
+
+class MuDist(object):
+    """
+    Precomputed interval means, medians, 1-norms and 2-norms
+    """
+    def __init__(self, y, p):
+        self.y = y
+        self.p = p
+        self.mu_memo = {}
+        self.dist_memo = {}
+        if p not in (1, 2):
+            raise ValueError("Invalid norm")
+        self._precomputed_2 = False
+        self.cum_absy = {}
+
+    def precompute(self, max_size, min_pos, max_pos):
+        y = self.y
+        if self.p == 1:
+            if (min_pos, min_pos+max_size) in self.mu_memo:
+                # not a full check, but most of the entries likely
+                # were already precalculated
+                return
+
+            # Precompute medians
+            for j in range(min_pos, max_pos):
+                medians = rolling_median(y[j:min(max_pos,(j+(max_size+1)))])
+                for p, m in enumerate(medians):
+                    if j+p > j+max_size:
+                        break
+                    self.mu_memo[j,j+p] = m
+
+        elif self.p == 2:
+            if self._precomputed_2:
+                return
+
+            cum_y = [0]
+            cum_y2 = [0]
+            s = 0
+            s2 = 0
+            for x in y:
+                s += x
+                s2 += x**2
+                cum_y.append(s)
+                cum_y2.append(s2)
+
+            self.cum_y = cum_y
+            self.cum_y2 = cum_y2
+            self._precomputed_2 = True
+
+    def get_funcs(self):
+        """
+        Interval distance and value functions d(l, r), mu(l, r)
+
+        The total work involved in computing all values is O(n)
+        for p=2, and O(n^2 log n) for p=1.
+
+        """
+        if self.p == 1:
+            y = self.y
+            cum_absy = self.cum_absy
+
+            @memoize(self.mu_memo)
+            def mu(l, r):
+                return median(y[l:r+1])
+
+            def cached_cumsum(m, l, r):
+                # Cache cumulative sums for each median, starting
+                # midway.  Getting amortized O(1) here relies on the
+                # leftmost point being accessed first.
+                cum_l, cumsum = cum_absy.setdefault(m, (l, [0]))
+                if l < cum_l:
+                    cumsum = [0]
+                    cum_absy[m] = (l, cumsum)
+                    cum_l = l
+                p = cum_l + len(cumsum)
+                if p > r+1:
+                    return cum_l, cumsum
+                s = cumsum[-1]
+                for j in range(p-1, r+1):
+                    s += abs(y[j] - m)
+                    cumsum.append(s)
+                return cum_l, cumsum
+
+            def dist(l, r):
+                m = mu(l, r)
+                cum_l, cumsum = cached_cumsum(m, l, r)
+                return cumsum[r+1-cum_l] - cumsum[l-cum_l]
+
+        elif self.p == 2:
+            if not self._precomputed_2:
+                self.precompute(None, None, None)
+
+            cum_y = self.cum_y
+            cum_y2 = self.cum_y2
+
+            def mu(l, r):
+                # mean(y[l:r+1])
+                return (cum_y[r+1] - cum_y[l]) / (r + 1 - l)
+
+            def dist(l, r):
+                # sum((y[l:r+1] - mean(y[l:r+1]))**2)
+                #
+                # This way to compute it is in principle susceptible
+                # to rounding errors, but we have to be O(1) fast here
+                return abs(cum_y2[r+1] - cum_y2[l] - (cum_y[r+1] - cum_y[l])**2 / (r + 1 - l))
+        else:
+            raise ValueError("Invalid value for p")
+
+        return mu, dist
+
+
+def median(items):
+    """Note: modifies the input list!"""
+    items.sort()
+    k = len(items)//2
+    if len(items) % 2 == 0:
+        return (items[k] + items[k - 1]) / 2
+    else:
+        return items[k]
+
+
+def rolling_median(items):
+    """Compute medians of [items[:1], items[:2], items[:3], ...] in O(n log n)"""
+    min_heap = []
+    max_heap = []
+    result = []
+    s = iter(items)
+    try:
+        while True:
+            v = -heapq.heappushpop(min_heap, -six.next(s))
+            heapq.heappush(max_heap, v)
+            result.append(max_heap[0])   # number of elements is odd
+
+            v = heapq.heappushpop(max_heap, six.next(s))
+            heapq.heappush(min_heap, -v)
+            result.append((max_heap[0] - min_heap[0])/2)   # number of elements is even
+    except StopIteration:
+        return result
+
+
+def golden_search(f, a, b, xatol=1e-6, ftol=1e-8):
+    """
+    Find minimum of a function on interval [a, b]
+    using golden section search
+    """
+
+    ratio = 2 / (1 + math.sqrt(5))
+
+    x0 = a
+    x3 = b
+    x1 = ratio * x0 + (1 - ratio) * x3
+    x2 = (1 - ratio) * x0 + ratio * x3
+
+    f1 = f(x1)
+    f2 = f(x2)
+
+    f0 = max(abs(f1), abs(f2))
+
+    while True:
+        if abs(x0 - x3) < xatol or abs(f1 - f2) < ftol*f0:
+            break
+
+        if f2 < f1:
+            x0 = x1
+            x1 = x2
+            x2 = ratio * x1 + (1 - ratio) * x3
+            f1 = f2
+            f2 = f(x2)
+        else:
+            x3 = x2
+            x2 = x1
+            x1 = ratio * x2 + (1 - ratio) * x0
+            f2 = f1
+            f1 = f(x1)
+
+    if f2 < f1:
+        return x2
+    else:
+        return x1
+
+
+def memoize(memo):
+    def deco(func):
+        def wrapper(*a):
+            r = memo.get(a)
+            if r is None:
+                r = func(*a)
+                memo[a] = r
+            return r
+        return wrapper
+    return deco
---
+++ test/test_step_detect.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import pytest
+
+from asv.step_detect import (solve_potts, solve_potts_autogamma, solve_potts_approx,
+                             detect_regressions, golden_search, median, rolling_median)
+
+
+try:
+    import numpy as np
+    HAVE_NUMPY = True
+except ImportError:
+    HAVE_NUMPY = False
+
+
+@pytest.mark.skip(not HAVE_NUMPY, reason="test needs numpy")
+def test_solve_potts():
+    np.random.seed(1234)
+
+    # Easy case, exact solver
+    y = [1, 1, 1, 2, 2, 2, 3, 3, 3]
+    right, values, dists = solve_potts(y, gamma=0.1)
+    assert right == [3, 6, 9]
+    assert np.allclose(values, [1, 2, 3], atol=0)
+
+    right, values, dists = solve_potts(y, gamma=8.0)
+    assert right == [9]
+    assert np.allclose(values, [2], atol=0)
+
+    # l1 norm
+    right, values, dists = solve_potts(y, gamma=0.1, p=1)
+    assert right == [3, 6, 9]
+    assert np.allclose(values, [1, 2, 3], atol=0)
+
+    # Bigger case, exact solver
+    t = np.arange(100)
+    y0 = (+ 0.4 * (t >= 5)
+          + 0.9 * (t >= 10)
+          - 0.2 * (t >= 20)
+          + 0.2 * (t >= 50)
+          + 1.1 * (t >= 70))
+    y = y0 + 0.1 * np.random.rand(y0.size)
+    right, values, dists = solve_potts(y.tolist(), gamma=0.1)
+    assert right == [5, 10, 20, 50, 70, 100]
+
+    # Bigger case, approximative solver
+    right, values, dists = solve_potts_approx(y.tolist(), gamma=0.1)
+    assert right == [5, 10, 20, 50, 70, 100]
+
+    # Larger case
+    t = np.arange(3000)
+    y0 = (+ 0.4 * (t >= 10)
+          + 0.9 * (t >= 30)
+          - 0.2 * (t >= 200)
+          + 0.2 * (t >= 600)
+          + 1.1 * (t >= 2500)
+          - 0.5 * (t >= 2990))
+
+    # Small amount of noise shouldn't disturb step finding
+    y = y0 + 0.05 * np.random.randn(y0.size)
+    right, values, dists, gamma = solve_potts_autogamma(y.tolist())
+    assert right == [10, 30, 200, 600, 2500, 2990, 3000]
+
+    # Large noise should prevent finding any steps
+    y = y0 + 3.0 * np.random.randn(y0.size)
+    right, values, dists, gamma = solve_potts_autogamma(y.tolist())
+    assert right == [3000]
+
+    # The routine shouldn't choke on datasets with 100k+ points.
+    # Appending noisy data to weakly noisy data should retain the
+    # steps in the former
+    y = y0 + 0.05 * np.random.rand(y.size)
+    ypad = 0.02 * np.random.randn(100000 - 3000)
+    right, values, dists, gamma = solve_potts_autogamma(y.tolist() + ypad.tolist())
+    assert right == [10, 30, 200, 600, 2500, 2990, 3000, 100000]
+
+
+@pytest.mark.skip(not HAVE_NUMPY, reason="test needs numpy")
+def test_detect_regressions():
+    for seed in [1234, 5678, 8901, 2345]:
+        np.random.seed(seed)
+        t = np.arange(4000)
+        y = 0.7 * np.random.rand(t.size)
+
+        y -= 0.3 * (t >= 1000)
+        y += 1.0 * (t >= 3234 + (seed % 123))
+
+        y = y.tolist()
+        y[123] = None
+        y[1234] = np.nan
+        regressions = detect_regressions(y)
+
+        assert len(regressions) == 1
+        pos, old_value, new_value = regressions[0]
+        assert pos == 3234 + (seed % 123)
+        assert np.allclose(old_value, 0.7/2 - 0.3, rtol=0.1, atol=0)
+        assert np.allclose(new_value, 0.7/2 - 0.3 + 1, rtol=0.1, atol=0)
+
+
+def test_golden_search():
+    def f(x):
+        return 1 + x**3 + x**4
+    x = golden_search(f, -1, -0.25, xatol=1e-5, ftol=0)
+    assert abs(x - (-3/4)) < 1e-4
+    x = golden_search(f, -0.25, 0.25, xatol=1e-5, ftol=0)
+    assert abs(x - (-0.25)) < 1e-4
+
+
+def test_rolling_median():
+    x = [1, 1, 10, 3, 5, 0.1, 0.6, -3, 4, 9]
+    medians = rolling_median(x)
+    for j, m in enumerate(medians):
+        assert abs(m - median(x[:j+1])) < 1e-10
