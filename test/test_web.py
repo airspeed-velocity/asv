fb33ddb16f3ab6b7a69789a58501e12eff11fd70
Pauli Virtanen <pav@iki.fi>
2015-05-16 23:03:20 +0300

    Add a functionality test for regressions.js

--- test/test_web.py
+++ test/test_web.py
@@ -5,6 +5,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import os
+import re
 import shutil
 import time
 import tempfile
@@ -89,3 +90,117 @@ def test_web_smoketest(browser, basic_html):
         time.sleep(1.0)
         error_box = browser.find_element_by_id('error-message')
         assert not error_box.is_displayed()
+
+
+def test_web_regressions(browser, tmpdir):
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    from selenium.webdriver import ActionChains
+
+    tmpdir = six.text_type(tmpdir)
+    local = abspath(dirname(__file__))
+    cwd = os.getcwd()
+
+    os.chdir(tmpdir)
+    try:
+        machine_file = join(tmpdir, 'asv-machine.json')
+
+        shutil.copyfile(join(local, 'asv-machine.json'),
+                        machine_file)
+
+        values = [[x]*2 for x in [1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2]]
+        dvcs = tools.generate_test_repo(tmpdir, values)
+        repo_path = dvcs.path
+
+        conf = config.Config.from_json({
+            'env_dir': join(tmpdir, 'env'),
+            'benchmark_dir': join(local, 'benchmark'),
+            'results_dir': join(tmpdir, 'results_workflow'),
+            'html_dir': join(tmpdir, 'html'),
+            'repo': repo_path,
+            'dvcs': 'git',
+            'project': 'asv',
+            'matrix': {}
+        })
+
+        Run.run(conf, range_spec="ALL", bench='params_examples.track_find_test',
+                _machine_file=machine_file, show_stderr=True, quick=True)
+        Publish.run(conf)
+    finally:
+        os.chdir(cwd)
+
+    bad_commit_hash = dvcs.get_hash('master~9')
+
+    with tools.preview(conf.html_dir) as base_url:
+        browser.get(base_url)
+
+        regressions_btn = browser.find_element_by_link_text('Show regressions')
+        regressions_btn.click()
+
+        # Check that the expected links appear in the table
+        regression_1 = browser.find_element_by_link_text('params_examples.track_find_test(1)')
+        regression_2 = browser.find_element_by_link_text('params_examples.track_find_test(2)')
+        bad_hash_link = browser.find_element_by_link_text(bad_commit_hash[:8])
+
+        href = regression_1.get_attribute('href')
+        assert '/#params_examples.track_find_test?' in href
+        assert 'time=' in href
+
+        # Check the contents of the table
+        table_rows = browser.find_elements_by_xpath('//table[1]/tbody/tr')
+        assert len(table_rows) == 2
+        cols1 = [td.text for td in table_rows[0].find_elements_by_xpath('td')]
+        cols2 = [td.text for td in table_rows[1].find_elements_by_xpath('td')]
+
+        assert cols1[0] == 'params_examples.track_find_test(1)'
+        assert cols2[0] == 'params_examples.track_find_test(2)'
+
+        assert re.match(r'^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d.\d+Z$', cols1[1])
+        assert re.match(r'^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d.\d+Z$', cols2[1])
+
+        assert cols1[2:] == [bad_commit_hash[:8], '2.00x', '1.00', '2.00', 'Ignore']
+        assert cols2[2:] == [bad_commit_hash[:8], '2.00x', '1.00', '2.00', 'Ignore']
+
+        # Check that the ignore buttons work as expected
+        buttons = [button for button in browser.find_elements_by_xpath('//button')
+                   if button.text == 'Ignore']
+        buttons[0].click()
+
+        # The button should disappear, together with the link
+        WebDriverWait(browser, 5).until_not(EC.visibility_of(buttons[0]))
+        WebDriverWait(browser, 5).until_not(EC.visibility_of(regression_1))
+
+        table_rows = browser.find_elements_by_xpath('//table[1]/tbody/tr')
+        assert len(table_rows) == 1
+
+        # There's a second button for showing the links, clicking
+        # which makes the elements reappear
+        show_button = [button for button in browser.find_elements_by_xpath('//button')
+                       if button.text == 'Show ignored regressions...'][0]
+        show_button.click()
+
+        regression_1 = browser.find_element_by_link_text('params_examples.track_find_test(1)')
+        WebDriverWait(browser, 5).until(EC.visibility_of(regression_1))
+
+        table_rows = browser.find_elements_by_xpath('//table[2]/tbody/tr')
+        assert len(table_rows) == 1
+
+        # There's an unignore button that moves the element back to the main table
+        unignore_button = [button for button in browser.find_elements_by_xpath('//button')
+                           if button.text == 'Unignore'][0]
+        unignore_button.click()
+
+        browser.find_elements_by_xpath('//table[1]/tbody/tr[2]') # wait until the table has two rows
+
+        table_rows = browser.find_elements_by_xpath('//table[1]/tbody/tr')
+        assert len(table_rows) == 2
+
+        # Check that a plot of some sort appears on mouseover
+        regression_1 = browser.find_element_by_link_text('params_examples.track_find_test(1)')
+
+        chain = ActionChains(browser)
+        chain.move_to_element(regression_1)
+        chain.perform()
+
+        popover = browser.find_element_by_css_selector('div.popover-content')
+        flotplot = browser.find_element_by_css_selector('canvas.flot-base')
