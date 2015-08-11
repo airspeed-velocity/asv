fcb65e5b3a10ee9f8f423d6eff266beb7032ee08
Christopher Whelan <topherwhelan@gmail.com>
2015-08-10 19:20:09 -0700

    Add utility function to enforce hash prefix uniqueness

--- test/test_results.py
+++ test/test_results.py
@@ -4,11 +4,13 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import os
 from os.path import join
 
 import six
 
-from asv import results
+from asv import results, util
+import pytest
 
 
 def test_results(tmpdir):
@@ -35,3 +37,25 @@ def test_results(tmpdir):
         assert r2._results == r._results
         assert r2.date == r.date
         assert r2.commit_hash == r.commit_hash
+
+
+def test_get_result_hash_from_prefix(tmpdir):
+    results_dir = tmpdir.mkdir('results')
+    machine_dir = results_dir.mkdir('machine')
+
+    for f in ['e5b6cdbc', 'e5bfoo12']:
+        open(join(str(machine_dir), '{0}-py2.7-Cython-numpy1.8.json'.format(f)), 'a').close()
+
+    # check unique, valid case
+    full_commit = results.get_result_hash_from_prefix(str(results_dir), 'machine', 'e5b6')
+    assert full_commit == 'e5b6cdbc'
+
+    # check invalid hash case
+    bad_commit = results.get_result_hash_from_prefix(str(results_dir), 'machine', 'foobar')
+    assert bad_commit is None
+
+    # check non-unique case
+    with pytest.raises(util.UserError) as excinfo:
+        results.get_result_hash_from_prefix(str(results_dir), 'machine', 'e')
+
+    assert 'one of multiple commits' in str(excinfo.value)
