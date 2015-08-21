bf49240d9e6d1c747a5a5392fa414d21a336b6fc
Pauli Virtanen <pav@iki.fi>
2015-08-21 22:06:06 +0300

    Generate summary web page only when it's requested
    This avoids loading the summary when following links that point directly
    to some graph.

--- asv/www/asv.js
+++ asv/www/asv.js
@@ -40,21 +40,6 @@ $(document).ready(function() {
             return prop;
     }
 
-    /* Callback a function when an element comes in view */
-    function callback_in_view(element, func) {
-        function handler(evt) {
-            var visible = (
-                (element.offset().top <= $(window).height() + $(window).scrollTop()) &&
-                    (element.offset().top + element.height() >= $(window).scrollTop()));
-            if (visible) {
-                func();
-                $(window).unbind('scroll', handler);
-            }
-        }
-        $(window).on('scroll', handler);
-        $(window).scroll();
-    }
-
     /*
       Parse hash string, assuming format similar to standard URL
       query strings
@@ -366,8 +351,6 @@ $(document).ready(function() {
             });
         });
 
-        make_summary();
-
         $('#log-scale').on('click', function(evt) {
             log_scale = !evt.target.classList.contains("active");
             reference_scale = false;
@@ -492,10 +475,7 @@ $(document).ready(function() {
             window_last_location = info.location.join('/');
 
             /* Redirect to correct handler */
-            if (info.location[0] == ['']) {
-                show_summary();
-            }
-            else if (show_page(info.location, info.params)) {
+            if (show_page(info.location, info.params)) {
                 /* show_page does the work */
             }
             else {
@@ -538,6 +518,10 @@ $(document).ready(function() {
 
         $(window).on('hashchange', hashchange);
 
+        $('#graph-display').hide();
+        $('#regressions-display').hide();
+        $('#summary-display').hide();
+
         hashchange();
     }).fail(function () {
         network_error();
@@ -547,101 +531,6 @@ $(document).ready(function() {
     $(window).resize(function() {
         update_graphs();
     });
-
-    function make_summary() {
-        var summary_display = $('#summary-display');
-
-        if (master_json.extra_pages) {
-            var pages_container = $('<div id="extra-buttons" class="btn-group" role="group" />');
-
-            $.each(master_json.extra_pages, function(j, item) {
-                var button = $('<a class="btn btn-default" role="button"/>');
-                button.attr('href', '#/' + item[0]);
-                button.text(item[1]);
-                button.tooltip({title: item[2], html: true});
-                pages_container.append(button);
-            });
-            pages_container.show();
-            summary_display.append(pages_container);
-        }
-
-        $.each(master_json.benchmarks, function(bm_name, bm) {
-            var container = $(
-                '<a class="btn benchmark-container" href="#' + bm_name +
-                '"/>');
-            var plot_div = $(
-                '<div id="summary-' + bm_name + '" class="benchmark-plot"/>');
-            var name = $('<div class="benchmark-text">' + bm_name + '</div>');
-            name.tooltip({
-                title: bm_name,
-                html: true,
-                placement: 'top',
-                container: 'body',
-                animation: false
-            });
-
-            plot_div.tooltip({
-                title: bm.code,
-                html: true,
-                placement: 'bottom',
-                container: 'body',
-                animation: false
-            });
-
-            container.append(name);
-            container.append(plot_div);
-            summary_display.append(container);
-
-            callback_in_view(plot_div, function() {
-                $.ajax({
-                    url: 'graphs/summary/' + bm_name + '.json',
-                    dataType: "json",
-                    cache: false
-                }).done(function(data) {
-                    var options = {
-                        colors: colors,
-                        series: {
-                            lines: {
-                                show: true,
-                                lineWidth: 2
-                            },
-                            shadowSize: 0
-                        },
-                        grid: {
-                            borderWidth: 1,
-                            margin: 0,
-                            labelMargin: 0,
-                            axisMargin: 0,
-                            minBorderMargin: 0
-                        },
-                        xaxis: {
-                            ticks: [],
-                        },
-                        yaxis: {
-                            ticks: [],
-                            min: 0
-                        },
-                        legend: {
-                            show: false
-                        }
-                    };
-
-                    var plot = $.plot(
-                        plot_div, [{data: data}], options);
-                }).fail(function() {
-                    // TODO: Handle failure
-                });
-            });
-        });
-    }
-
-    function show_summary() {
-        $('#graph-display').hide();
-        $('#regressions-display').hide();
-        $('#summary-display').show();
-        $("#title").text("All benchmarks");
-        $('.tooltip').remove();
-    }
 
     function setup_benchmark_params(state_selection, sub_benchmark_idx) {
         if (!current_benchmark) {
@@ -1666,8 +1555,6 @@ $(document).ready(function() {
         update_range();
     }
 
-    show_summary();
-
 
     /*
       Dealing with sub-pages
@@ -1697,6 +1584,8 @@ $(document).ready(function() {
 
     this.pretty_second = pretty_second;
     this.time_units = time_units;
+
+    this.colors = colors;
 
     $.asv = this;
 });
