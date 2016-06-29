airspeed-velocity/asv

commit 1554f80306ff2e95e69fcfda3a25c21a9b5b1f85

Add a way to change attributes of TimeBenchmark for parameterized benchmarks

asv/benchmark.py
@@ -374,11 +374,20 @@ class TimeBenchmark(Benchmark):
         Benchmark.__init__(self, name, func, attr_sources)
         self.type = "time"
         self.unit = "seconds"
-        self.goal_time = _get_first_attr(attr_sources, 'goal_time', 2.0)
-        self.timer = _get_first_attr(attr_sources, 'timer', process_time)
+        self._attr_sources = attr_sources
+        self._load_vars()
+
+    def _load_vars(self):
         self.repeat = _get_first_attr(
-            attr_sources, 'repeat', timeit.default_repeat)
-        self.number = int(_get_first_attr(attr_sources, 'number', 0))
+            self._attr_sources, 'repeat', timeit.default_repeat)
+        self.number = int(_get_first_attr(self._attr_sources, 'number', 0))
+        self.goal_time = _get_first_attr(self._attr_sources, 'goal_time', 2.0)
+        self.timer = _get_first_attr(self._attr_sources, 'timer', process_time)
+
+    def do_setup(self):
+        Benchmark.do_setup(self)
+        # For parameterized tests, setup() is allowed to change these
+        self._load_vars()
 
     def run(self, *param):
         number = self.number

test/test_benchmarks.py
@@ -40,19 +40,19 @@ def test_find_benchmarks(tmpdir):
     assert len(b) == 3
 
     b = benchmarks.Benchmarks(conf, regex='example')
-    assert len(b) == 9
+    assert len(b) == 10
 
     b = benchmarks.Benchmarks(conf, regex='time_example_benchmark_1')
     assert len(b) == 2
 
     b = benchmarks.Benchmarks(conf)
-    assert len(b) == 13
+    assert len(b) == 14
 
     envs = list(environment.get_environments(conf))
     b = benchmarks.Benchmarks(conf)
     times = b.run_benchmarks(envs[0], profile=True, show_stderr=True)
 
-    assert len(times) == 13
+    assert len(times) == 14
     assert times[
         'time_examples.TimeSuite.time_example_benchmark_1']['result'] is not None
     # Benchmarks that raise exceptions should have a time of "None"
@@ -79,6 +79,8 @@ def test_find_benchmarks(tmpdir):
 
     assert times['params_examples.ParamSuite.track_value']['result']['params'] == [["'a'", "'b'", "'c'"]]
     assert times['params_examples.ParamSuite.track_value']['result']['result'] == [1+0, 2+0, 3+0]
+
+    assert isinstance(times['params_examples.TuningTest.time_it']['result']['result'][0], float)
 
     profile_path = os.path.join(tmpdir, 'test.profile')
     with open(profile_path, 'wb') as fd:

test/benchmark/params_examples.py
@@ -37,3 +37,23 @@ class ParamSuite:
     def teardown(self, p):
         self.count += 1
         del self.value
+
+
+class TuningTest:
+    params = [1, 2]
+    counter = [0]
+    number = 10
+    repeat = 10
+
+    def setup(self, n):
+        self.number = 1
+        self.repeat = n
+        self.counter[0] = 0
+
+    def time_it(self, n):
+        self.counter[0] += 1
+
+    def teardown(self, n):
+        # The time benchmark may call it one additional time
+        if not (n <= self.counter[0] <= n + 1):
+            raise RuntimeError("Number and repeat didn't have effect")

commit 628cdcf7020178ac8bad832883b1c9b669bbbc6b

Fix --quick option
Also add a test for it. It was broken by 1554f80306

asv/benchmark.py
@@ -420,8 +420,10 @@ class Benchmark(object):
             benchmark.set_param_idx(param_idx)
 
         if quick:
-            benchmark.repeat = 1
-            benchmark.number = 1
+            class QuickBenchmarkAttrs:
+                repeat = 1
+                number = 1
+            benchmark._attr_sources.insert(0, QuickBenchmarkAttrs)
 
         return benchmark
 

test/test_benchmarks.py
@@ -240,3 +240,32 @@ def track_this():
 
     b = benchmarks.Benchmarks(conf, repo, envs, regex='track_this')
     assert len(b) == 1
+
+
+def test_quick(tmpdir):
+    # Check that the quick option works
+    tmpdir = six.text_type(tmpdir)
+    os.chdir(tmpdir)
+
+    shutil.copytree(BENCHMARK_DIR, 'benchmark')
+
+    d = {}
+    d.update(ASV_CONF_JSON)
+    d['env_dir'] = "env"
+    d['benchmark_dir'] = 'benchmark'
+    d['repo'] = tools.generate_test_repo(tmpdir, [0]).path
+    conf = config.Config.from_json(d)
+
+    repo = get_repo(conf)
+    envs = list(environment.get_environments(conf, None))
+
+    b = benchmarks.Benchmarks(conf, repo, envs)
+    skip_names = [name for name in b.keys() if name != 'time_examples.TimeWithRepeat.time_it']
+    times = b.run_benchmarks(envs[0], quick=True, show_stderr=True, skip=skip_names)
+
+    assert len(times) == 1
+
+    # Check that the benchmark was run only once. The result for quick==False
+    # is tested above in test_find_benchmarks
+    expected = ["<1>"]
+    assert times['time_examples.TimeWithRepeat.time_it']['stderr'].split() == expected