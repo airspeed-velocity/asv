ed33725ef2d38cfa7b46afe2055cc64556e6d49c
Chris Warburton <chriswarbo@gmail.com>
2017-06-21 00:36:43 +0100

    Use for loop as suggested, use python 2/3 octals

--- asv/commands/publish.py
+++ asv/commands/publish.py
@@ -89,9 +89,10 @@ class Publish(Command):
         shutil.copytree(template_dir, conf.html_dir)
 
         # Ensure html_dir is writable even if template_dir is on a read-only FS
-        os.chmod(conf.html_dir, 0755)
-        [[os.chmod(os.path.join(pre, x), 0755) for x in (fs + ds)]
-         for (pre, ds, fs) in os.walk(conf.html_dir)]
+        os.chmod(conf.html_dir, 0o755)
+        for (pre, ds, fs) in os.walk(conf.html_dir):
+            for x in fs + ds:
+                os.chmod(os.path.join(pre, x), 0o755)
 
         log.step()
         log.info("Loading machine info")
