d8a6abb7ca1bf2ada6bad24e0c9311f533dc310e
Pauli Virtanen <pav@iki.fi>
2018-06-02 14:13:20 +0000

    Backport pull request #646 from jni/conda-4.4
    BUG: find conda executable from CONDA_EXE before PATH
    
    In conda >= 4.4, conda is shipped with a conda bash function to enable the
    syntax "conda activate env-name", available also when conda is not in PATH.
    The path to the conda executable is in this case found in the environment variable
    CONDA_EXE, so make asv use it if it's present.
    
    (cherry picked from commit e1903152317873ea70b15331dd4c4a20e912f5fc)

--- asv/plugins/conda.py
+++ asv/plugins/conda.py
@@ -18,6 +18,37 @@ from .. import util
 WIN = (os.name == "nt")
 
 
+def _find_conda():
+    """Find the conda executable robustly across conda versions.
+
+    Returns
+    -------
+    conda : str
+        Path to the conda executable.
+
+    Raises
+    ------
+    IOError
+        If the executable cannot be found in either the CONDA_EXE environment
+        variable or in the PATH.
+
+    Notes
+    -----
+    In POSIX platforms in conda >= 4.4, conda can be set up as a bash function
+    rather than an executable. (This is to enable the syntax
+    ``conda activate env-name``.) In this case, the environment variable
+    ``CONDA_EXE`` contains the path to the conda executable. In other cases,
+    we use standard search for the appropriate name in the PATH.
+
+    See https://github.com/airspeed-velocity/asv/issues/645 for more details.
+    """
+    if 'CONDA_EXE' in os.environ:
+        conda = os.environ['CONDA_EXE']
+    else:
+        conda = util.which('conda')
+    return conda
+
+
 class Conda(environment.Environment):
     """
     Manage an environment using conda.
@@ -60,7 +91,7 @@ class Conda(environment.Environment):
             return False
 
         try:
-            conda = util.which('conda')
+            conda = _find_conda()
         except IOError:
             return False
         else:
@@ -85,7 +116,7 @@ class Conda(environment.Environment):
 
     def _setup(self):
         try:
-            conda = util.which('conda')
+            conda = _find_conda()
         except IOError as e:
             raise util.UserError(str(e))
 
