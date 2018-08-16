124e894d38503f4e4393ab7e92dbddb4396d56e0
Pauli Virtanen <pav@iki.fi>
2018-08-16 21:27:36 +0200

    Put vendorized files to a separate submodule repository
    Downloading the asset files in setup.py can fail, and it is better to
    force the download go via "git clone --recursive".

--- setup.py
+++ setup.py
@@ -11,27 +11,9 @@ from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatfo
 
 
 import os
-import shutil
 import subprocess
 import sys
 import ast
-try:
-    from urllib2 import urlopen
-except ImportError:
-    from urllib.request import urlopen
-
-
-VENDOR_ASSETS = {
-    "jquery-1.11.0.min.js": "https://code.jquery.com/jquery-1.11.0.min.js",
-    "jquery.flot-0.8.2.min.js": "https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.2/jquery.flot.min.js",
-    "jquery.flot-0.8.2.time.min.js": "https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.2/jquery.flot.time.min.js",
-    "jquery.flot-0.8.2.selection.min.js": "https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.2/jquery.flot.selection.min.js",
-    "jquery.flot-0.8.2.categories.min.js": "https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.2/jquery.flot.categories.min.js",
-    "bootstrap-3.1.0.min.js": "https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js",
-    "bootstrap-3.1.0.min.css": "https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css"
-}
-
-VENDOR_DIR = os.path.join(os.path.dirname(__file__), 'asv', 'www', 'vendor')
 
 
 # A py.test test command
@@ -171,29 +153,6 @@ class optional_build_ext(build_ext):
             raise BuildFailed()
 
 
-def download_assets():
-    if not os.path.isdir(VENDOR_DIR):
-        os.makedirs(VENDOR_DIR)
-
-    for fn, asset in sorted(VENDOR_ASSETS.items()):
-        dst = os.path.join(VENDOR_DIR, fn)
-
-        if os.path.isfile(dst):
-            continue
-
-        print("Downloading {0} to asv/www/vendor...".format(asset))
-
-        fsrc = urlopen(asset)
-        try:
-            with open(dst + ".new", 'wb') as fdst:
-                shutil.copyfileobj(fsrc, fdst)
-        finally:
-            fsrc.close()
-
-        # Usually atomic
-        os.rename(dst + ".new", dst)
-
-
 def run_setup(build_binary=False):
     version = get_version()
     git_hash = get_git_hash()
@@ -206,9 +165,6 @@ def run_setup(build_binary=False):
 
     write_version_file(os.path.join(basedir, 'asv', '_version.py'),
                        suffix, git_hash)
-
-    # Download missing assets
-    download_assets()
 
     # Install entry points for making releases with zest.releaser
     entry_points = {}
