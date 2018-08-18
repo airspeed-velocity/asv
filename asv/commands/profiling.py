96f380c3ba0bfba19009c31674ce78d35c86c2bb
Pauli Virtanen <pav@iki.fi>
2018-08-18 20:08:18 +0200

    profile: fix cosmetic issues in printout

--- asv/commands/profiling.py
+++ asv/commands/profiling.py
@@ -13,7 +13,7 @@ import tempfile
 
 from . import Command
 from ..benchmarks import Benchmarks
-from ..console import log
+from ..console import log, color_print
 from ..environment import get_environments, is_existing_only
 from ..machine import Machine
 from ..profiling import ProfilerGui
@@ -192,6 +192,8 @@ class Profile(Command):
 
                 profile_data = results.get_profile(benchmark_name)
 
+        log.flush()
+
         if gui is not None:
             log.debug("Opening gui {0}".format(gui))
             with temp_profile(profile_data) as profile_path:
@@ -200,6 +202,7 @@ class Profile(Command):
             with io.open(output, 'wb') as fd:
                 fd.write(profile_data)
         else:
+            color_print('')
             with temp_profile(profile_data) as profile_path:
                 stats = pstats.Stats(profile_path)
                 stats.sort_stats('cumulative')
