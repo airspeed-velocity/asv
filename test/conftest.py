9ab99ded58b8d5a402bd68a0b81d1edd66bd5244
Pauli Virtanen <pav@iki.fi>
2019-01-01 15:07:22 +0200

    Give up offline Conda in tests, seems to get too complicated

--- test/conftest.py
+++ test/conftest.py
@@ -9,13 +9,6 @@ def pytest_addoption(parser):
                            "with a return statement with selenium.webdriver object, for "
                            "example 'return Chrome()'"))
 
-    parser.addoption("--offline", action="store_true", default=False,
-                     help=("Do not download items from internet. Use if you have predownloaded "
-                           "packages with Conda. (Virtualenv tests are always offline.)"))
-
 
 def pytest_sessionstart(session):
     os.environ['PIP_NO_INDEX'] = '1'
-
-    if session.config.getoption('offline'):
-        os.environ['CONDA_OFFLINE'] = 'True'
