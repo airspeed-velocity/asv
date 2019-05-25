e2adc31ad8255e424e7d9610ec66f623c827ab68
Pauli Virtanen <pav@iki.fi>
2019-05-25 17:17:06 +0300

    Reduce duplication in Conda._setup

--- asv/plugins/conda.py
+++ asv/plugins/conda.py
@@ -124,11 +124,18 @@ class Conda(environment.Environment):
 
         log.info("Creating conda environment for {0}".format(self.name))
 
-        if not self._conda_environment_file:
+        if self._conda_environment_file:
+            # user-provided file
+            env_file = None
+            env_file_name = self._conda_environment_file
+        else:
             # create a temporary environment.yml file
             # and use that to generate the env for benchmarking
             env_file = tempfile.NamedTemporaryFile(mode='w', delete=False, suffix=".yml")
-            try:
+            env_file_name = env_file.name
+
+        try:
+            if env_file is not None:
                 env_file.write('name: {0}\n'
                                'channels:\n'.format(self.name))
                 env_file.writelines(('   - %s\n' % ch for ch in self._conda_channels))
@@ -148,24 +155,19 @@ class Conda(environment.Environment):
 
                 env_file.close()
 
-                util.check_output([conda] + ['env', 'create', '-f', env_file.name,
-                                             '-p', self._path, '--force'])
-            except Exception as exc:
-                if os.path.isfile(env_file.name):
-                    with open(env_file.name, 'r') as f:
-                        text = f.read()
-                    log.info("conda env create failed: in {} with:\n{}".format(self._path, text))
-                raise
-            finally:
+            util.check_output([conda] + ['env', 'create', '-f', env_file_name,
+                                         '-p', self._path, '--force'])
+        except Exception:
+            if env_file is None:
+                log.info("conda env create failed: in {} with file {}".format(self._path, env_file_name))
+            elif os.path.isfile(env_file_name):
+                with open(env_file_name, 'r') as f:
+                    text = f.read()
+                log.info("conda env create failed: in {} with:\n{}".format(self._path, text))
+            raise
+        finally:
+            if env_file is not None:
                 os.unlink(env_file.name)
-        else:
-            try:
-                util.check_output([conda] + ['env', 'create', '-f', self._conda_environment_file,
-                                             '-p', self._path, '--force'])
-            except Exception as exc:
-                log.info("conda env create failed: in {} with environment file: {}".format(
-                    self._path, self._conda_environment_file))
-                raise
 
     def _get_requirements(self, conda):
         if self._requirements:
