05043d5462ca99f69aa8a73db14f642760313244
mariam <mariamnb6@gmail.com>
2022-02-26 15:05:11 +0300

    remove imports from __future__ module3

--- asv/extern/asizeof.py
+++ asv/extern/asizeof.py
@@ -191,7 +191,8 @@ if sys.version_info < (2, 6, 0):
     raise NotImplementedError('%s requires Python 2.6 or newer' % ('asizeof',))
 
 # all imports listed explicitly to help PyChecker
-
+from inspect import (isbuiltin, isclass, iscode, isframe, isfunction,
+                     ismethod, ismodule, stack)
 from math import log
 from os import curdir, linesep
 from struct import calcsize  # type/class Struct only in Python 2.5+
