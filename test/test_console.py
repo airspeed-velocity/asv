10feb10ae5922a90ace757902c113b7caf7288e1
dorothykiz1 <kizdorothy@gmail.com>
2022-03-02 18:51:19 +0300

    remove asv/commands/quickstart.py and test/test_console.py files

--- test/test_console.py
+++ test/test_console.py
@@ -1,11 +1,14 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
+import six
 import io
 import os
 import sys
 import locale
 import itertools
+
 from asv.console import _write_with_fallback, color_print, log
+
 import pytest
 
 
@@ -18,39 +21,53 @@ def test_write_with_fallback(tmpdir, capfd):
             locale.getpreferredencoding = lambda: preferred_encoding
 
             # Check writing to io.StringIO
-            stream = io.StringIO()
-            _write_with_fallback(value, stream)
-            assert stream.getvalue() == value
+            if six.PY3:
+                stream = io.StringIO()
+                _write_with_fallback(value, stream)
+                assert stream.getvalue() == value
+            else:
+                stream = io.StringIO()
+                with pytest.raises(TypeError):
+                    _write_with_fallback(value, stream)
+
             # Check writing to a text stream
-            buf = io.BytesIO()
-            stream = io.TextIOWrapper(buf, encoding=stream_encoding)
-            _write_with_fallback(value, stream)
-            stream.flush()
-            got = buf.getvalue()
-            assert got == expected
+            if six.PY3:
+                buf = io.BytesIO()
+                stream = io.TextIOWrapper(buf, encoding=stream_encoding)
+                _write_with_fallback(value, stream)
+                stream.flush()
+                got = buf.getvalue()
+                assert got == expected
 
             # Writing to io.BytesIO
-            stream = io.BytesIO()
-            with pytest.raises(TypeError):
+            if six.PY3:
+                stream = io.BytesIO()
+                with pytest.raises(TypeError):
+                    _write_with_fallback(value, stream)
+            else:
+                stream = io.BytesIO()
                 _write_with_fallback(value, stream)
+                assert stream.getvalue() == expected
 
             # Check writing to a file
             fn = os.path.join(tmpdir, 'tmp.txt')
-            with io.open(fn, 'w', encoding=stream_encoding) as stream:
-                _write_with_fallback(value, stream)
-            with open(fn, 'rb') as stream:
-                got = stream.read()
-                assert got == expected
+            if six.PY3:
+                with io.open(fn, 'w', encoding=stream_encoding) as stream:
+                    _write_with_fallback(value, stream)
+                with open(fn, 'rb') as stream:
+                    got = stream.read()
+                    assert got == expected
 
             # Check writing to Py2 files
-            if stream_encoding == preferred_encoding:
-                # No stream encoding: write in locale encoding
-                for mode in ['w', 'wb']:
-                    with open(fn, mode) as stream:
-                        _write_with_fallback(value, stream)
-                    with open(fn, 'rb') as stream:
-                        got = stream.read()
-                        assert got == expected
+            if not six.PY3:
+                if stream_encoding == preferred_encoding:
+                    # No stream encoding: write in locale encoding
+                    for mode in ['w', 'wb']:
+                        with open(fn, mode) as stream:
+                            _write_with_fallback(value, stream)
+                        with open(fn, 'rb') as stream:
+                            got = stream.read()
+                            assert got == expected
         finally:
             locale.getpreferredencoding = old_getpreferredencoding
 
@@ -67,7 +84,10 @@ def test_write_with_fallback(tmpdir, capfd):
 
     for pref_enc, stream_enc, s in itertools.product(encodings, encodings, strings):
         expected = None
-        encodings = [stream_enc, pref_enc]
+        if six.PY3:
+            encodings = [stream_enc, pref_enc]
+        else:
+            encodings = [pref_enc]
         for enc in encodings:
             try:
                 expected = s.encode(enc)
