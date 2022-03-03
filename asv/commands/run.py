0f89f3386e11154874a20e25aa03275f090cb3ca
LucyJimenez <lucy.jimenez.chem@gmail.com>
2022-03-02 21:17:16 -0500

    changes to fix flake8 issues on run.py

--- asv/commands/run.py
+++ asv/commands/run.py
@@ -238,8 +238,8 @@ class Run(Command):
 
         if range_spec is None:
             try:
-                commit_hashes = list(set([(repo.get_hash_from_name(branch) for
-                                        branch in conf.branches)]))
+                commit_hashes = list(set([repo.get_hash_from_name(branch) for
+                                         branch in conf.branches]))
             except NoSuchNameError as exc:
                 raise util.UserError('Unknown branch {0} in configuration'.format(exc))
         elif range_spec == 'EXISTING':
@@ -466,8 +466,9 @@ class Run(Command):
                         params['python'] = env.python
                         params.update(env.requirements)
 
-                        skip_save = dry_run or (isinstance(env, environment.ExistingEnvironment)
-                                                and set_commit_hash is None)
+                        skip_save = (dry_run or
+                                     (isinstance(env, environment.ExistingEnvironment) and
+                                      set_commit_hash is None))
 
                         skip_list = skipped_benchmarks[(commit_hash, env.name)]
                         benchmark_set = benchmarks.filter_out(skip_list)
@@ -521,7 +522,8 @@ class Run(Command):
 
                         if durations > 0:
                             duration_set = Show._get_durations([(machine, result)], benchmark_set)
-                            log.info(cls.format_durations(duration_set[(machine, env.name)], durations))
+                            log.info(cls.format_durations(
+                                duration_set[(machine, env.name)], durations))
 
         if failures and strict:
             return 2
