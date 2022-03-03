083dcb7e09525b090a1ceffbfb9d75593dbf49fc
LucyJimenez <lucy.jimenez.chem@gmail.com>
2022-03-03 08:17:47 -0500

    fix issues pep8 related

--- asv/plugins/virtualenv.py
+++ asv/plugins/virtualenv.py
@@ -48,7 +48,7 @@ class Virtualenv(environment.Environment):
                                          tagged_env_vars)
 
         try:
-            import virtualenv
+            import virtualenv # noqa F401 unused, but required in case the step before don't work
         except ImportError:
             raise environment.EnvironmentUnavailable(
                 "virtualenv package not installed")
