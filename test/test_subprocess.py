2ca3eb7bb1fd74f95560d5b32a5a5bf51911f40c
LucyJimenez <lucy.jimenez.chem@gmail.com>
2022-03-03 23:33:10 -0500

    check use of noqa

--- test/test_subprocess.py
+++ test/test_subprocess.py
@@ -34,8 +34,7 @@ sys.stderr.write("Stderr before waiting\n")
 sys.stdout.flush()
 sys.stderr.flush()
 subprocess.call([sys.executable, "-c",
-    "import sys,
-     subprocess; subprocess.call([sys.executable, '-c', 'import time; time.sleep(360)'])"])
+    "import sys, subprocess; subprocess.call([sys.executable, '-c', 'import time; time.sleep(360)'])"]) # noqa
 sys.stdout.write("Stdout after waiting\n")
 sys.stderr.write("Stderr after waiting\n")
     """)
