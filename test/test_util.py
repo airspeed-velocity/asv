9af7f276856269047a2e753626a7a64cf879529b
LucyJimenez <lucy.jimenez.chem@gmail.com>
2022-03-08 09:52:42 -0500

    fix pepe8 issue and address issue

--- test/test_util.py
+++ test/test_util.py
@@ -1,5 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-import locale # noqa F401 imported but unused
+import locale # noqa F401 imported but unused see #1106
 import os
 import sys
 import shutil
