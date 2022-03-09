f76ea6a192a586e5674724b3c41191249dc695e6
dorothykiz1 <kizdorothy@gmail.com>
2022-03-09 19:49:15 +0300

    fix import errors

--- test/conftest.py
+++ test/conftest.py
@@ -1,28 +1,27 @@
+import contextlib
 import os
 import shutil
-import contextlib
+import textwrap
+from os.path import abspath, dirname, join
+
 import pytest
 import selenium
-import textwrap
-from os.path import join, abspath, dirname
-from asv import config
-from asv import repo
-from .test_workflow import generate_basic_conf
-from .tools import (locked_cache_dir, run_asv_with_conf, _build_dummy_wheels,
-                    WAIT_TIME, DUMMY1_VERSION, DUMMY2_VERSIONS, WIN, HAS_CONDA,
-                    PYTHON_VER1, PYTHON_VER2)
-from .test_web import _rebuild_basic_html
-from asv.repo import get_repo
-from .test_benchmarks import ASV_CONF_JSON, BENCHMARK_DIR
-from asv import environment
 
+from asv import config, environment, repo
+from asv.repo import get_repo
+
+from . import tools
+from .test_benchmarks import ASV_CONF_JSON, BENCHMARK_DIR
+from .test_web import _rebuild_basic_html
+from .test_workflow import generate_basic_conf
+from .tools import (DUMMY1_VERSION, DUMMY2_VERSIONS, HAS_CONDA, PYTHON_VER1,
+                    PYTHON_VER2, WAIT_TIME, WIN, _build_dummy_wheels,
+                    locked_cache_dir, run_asv_with_conf)
 
 try:
     import hglib
 except ImportError:
     hglib = None
-
-from . import tools
 
 
 def pytest_addoption(parser):
