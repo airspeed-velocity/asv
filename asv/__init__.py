97716c60b7ac2b1469bffc169b47a9ad6b81497e
Fangchen Li <fangchen.li@outlook.com>
2022-03-10 21:51:28 -0600

    BLD: use setuptools-scm for versioning

--- asv/__init__.py
+++ asv/__init__.py
@@ -1,28 +1,4 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-import os
-import sys
-
-# This __version__ assignment is parsed by setup.py; keep it in this form.
-# Development versions end with ".dev" (suffix is added below).
-__version__ = "0.6.dev"
-__release__ = not __version__.endswith(".dev")
-
-try:
-    from ._version import __githash__, __suffix__
-except ImportError:
-    __githash__ = None
-    __suffix__ = "0"
-if not __release__:
-    __version__ += __suffix__
-del __suffix__
-
-
-if sys.version_info >= (3, 3):
-    # OS X framework builds of Python 3.3 can not call other 3.3
-    # virtualenvs as a subprocess because `__PYENV_LAUNCHER__` is
-    # inherited.
-    if os.environ.get('__PYVENV_LAUNCHER__'):
-        os.unsetenv('__PYVENV_LAUNCHER__')
-
-from . import plugin_manager  # noqa F401 unused, but required to load plugins
+from asv._version import version as __version__
+from asv import plugin_manager  # noqa F401 unused, but required to load plugins
