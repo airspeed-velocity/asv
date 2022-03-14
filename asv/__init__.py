bccfad9c589243e4fed4d9b4906998f15d22ebf6
dorothykiz1 <kizdorothy@gmail.com>
2022-03-14 22:20:00 +0300

    clean up setup.cfg file and flake8 errors

--- asv/__init__.py
+++ asv/__init__.py
@@ -1,4 +1,4 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-from asv._version import version as __version__
-from asv import plugin_manager
+from asv._version import version as __version__ # noqa see #1145
+from asv import plugin_manager # noqa see #1145
