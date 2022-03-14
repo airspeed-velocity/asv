bf862bb2f0094e69695485d5275086648d8d24d3
Fangchen Li <fangchen.li@outlook.com>
2022-03-14 18:00:19 -0500

    CLN: remove submodule, fetch js file from cdn (#1149)

--- setup.py
+++ setup.py
@@ -1,39 +1,8 @@
 #!/usr/bin/env python
-import os
-import subprocess
 from distutils.errors import CompileError
 
 from setuptools import setup, Extension
-from setuptools.command.sdist import sdist
 from setuptools.command.build_ext import build_ext
-
-
-class sdist_checked(sdist):
-    """Check git submodules on sdist to prevent incomplete tarballs"""
-    def run(self):
-        self.__check_submodules()
-        sdist.run(self)
-
-    def __check_submodules(self):
-        """
-        Verify that the submodules are checked out and clean.
-        """
-        if not os.path.exists('.git'):
-            return
-        with open('.gitmodules') as f:
-            for l in f:
-                if 'path' in l:
-                    p = l.split('=')[-1].strip()
-                    if not os.path.exists(p):
-                        raise ValueError('Submodule %s missing' % p)
-
-        proc = subprocess.Popen(['git', 'submodule', 'status'],
-                                stdout=subprocess.PIPE)
-        status, _ = proc.communicate()
-        status = status.decode("ascii", "replace")
-        for line in status.splitlines():
-            if line.startswith('-') or line.startswith('+'):
-                raise ValueError('Submodule not clean: %s' % line)
 
 
 class optional_build_ext(build_ext):
@@ -48,5 +17,5 @@ class optional_build_ext(build_ext):
 if __name__ == "__main__":
     setup(
         ext_modules=[Extension("asv._rangemedian", ["asv/_rangemedian.cpp"])],
-        cmdclass={"build_ext": optional_build_ext, "sdist": sdist_checked},
+        cmdclass={"build_ext": optional_build_ext},
     )
