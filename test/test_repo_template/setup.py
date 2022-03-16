6ba4ba486be6d5975d19aa1ac596db9b27b82f41
Marc Garcia <garcia.marc@gmail.com>
2022-03-16 01:13:38 +0000

    Update test/test_repo_template/setup.py

--- test/test_repo_template/setup.py
+++ test/test_repo_template/setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 with open('asv_test_repo/build_time_env.py', 'w') as f:
-    f.write('env = {}')
+    f.write("env = {{}}\n".format(repr(dict(os.environ))))
 
 setup(name='asv_test_repo',
       version="{version}",
