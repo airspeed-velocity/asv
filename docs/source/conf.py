93faea0934a0cc3e70f19aa54764624702c87819
Mark Harfouche <mark.harfouche@gmail.com>
2022-11-20 17:06:20 -0500

    Update copyright to 2022

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'airspeed velocity'
-copyright = u'2013-2018, Michael Droettboom, Pauli Virtanen'
+copyright = u'2013-2022, Michael Droettboom, Pauli Virtanen'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
