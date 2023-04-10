a63e5d0cb1a617b45564f75c5a3dca0b64ab4a47
Rohit Goswami <rog32@hi.is>
2023-04-10 02:21:29 +0000

    MAINT: Stop disabling indexes in tests

--- test/conftest.py
+++ test/conftest.py
@@ -106,7 +106,7 @@ def generate_basic_conf(tmpdir,
 
 
 def pytest_sessionstart(session):
-    os.environ['PIP_NO_INDEX'] = '1'
+    # os.environ['PIP_NO_INDEX'] = '0'
     _monkeypatch_conda_lock(session.config)
 
     # Unregister unwanted environment types
