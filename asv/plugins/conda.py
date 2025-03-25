c8926a5f6ec762828c02a31e3f0eb4b3732038bf
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2025-03-25 09:34:38 +0100

    MAINT: Apply ruff/pyupgrade rule UP034
    UP034 Avoid extraneous parentheses

--- asv/plugins/conda.py
+++ asv/plugins/conda.py
@@ -143,11 +143,11 @@ class Conda(environment.Environment):
         env_file = tempfile.NamedTemporaryFile(mode='w', delete=False, suffix=".yml")
         try:
             env_file.write(f'name: {self.name}\nchannels:\n')
-            env_file.writelines((f'   - {ch}\n' for ch in self._conda_channels))
+            env_file.writelines(f'   - {ch}\n' for ch in self._conda_channels)
             if conda_args:
                 env_file.write('dependencies:\n')
                 # categorize & write dependencies based on pip vs. conda
-                env_file.writelines((f'   - {s}\n' for s in conda_args))
+                env_file.writelines(f'   - {s}\n' for s in conda_args)
             env_file.close()
             try:
                 env_file_name = self._conda_environment_file or env_file.name
