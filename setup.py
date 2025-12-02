2a02dd50f137db63b37f5a8ae56c6443a4e10299
mattip <matti.picus@gmail.com>
2025-12-02 02:37:29 +0200

    cannot build free-threaded limited API wheels [wheel builds]

--- setup.py
+++ setup.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
 
 from setuptools import Extension, setup
+import sysconfig
 
 
 if __name__ == "__main__":
-    setup(
-        ext_modules=[
-            Extension(
-                "asv._rangemedian",
-                sources=["asv/_rangemedian.cpp"],
-                define_macros=[("Py_LIMITED_API", "0x03060000")],
-                py_limited_api=True,
-            )
-        ],
-        options={"bdist_wheel": {"py_limited_api": "cp36"}},
-    )
+    if sysconfig.get_config_var("Py_GIL_DISABLED") == 1:
+        # Cannot build a limited API extension module for free-threaded builds
+        setup(
+            ext_modules=[
+                Extension(
+                    "asv._rangemedian",
+                    sources=["asv/_rangemedian.cpp"],
+                )
+            ],
+        )
+    else:
+        setup(
+            ext_modules=[
+                Extension(
+                    "asv._rangemedian",
+                    sources=["asv/_rangemedian.cpp"],
+                    define_macros=[("Py_LIMITED_API", "0x03060000")],
+                    py_limited_api=True,
+                )
+            ],
+            options={"bdist_wheel": {"py_limited_api": "cp36"}},
+        )
+    else:
